f0260102a812944f60a9da588463c77cce426ff9
dlzh <1173495728@qq.com>
2015-12-28 00:19:38 +0800

    添加jar

--- app/src/main/java/com/lizhidu/appdesign/MyApplication.java
+++ app/src/main/java/com/lizhidu/appdesign/MyApplication.java
@@ -1,10 +1,20 @@
 package com.lizhidu.appdesign;
 
 import android.app.Application;
+import android.content.Context;
+import android.util.Log;
 
 
 import com.lizhidu.appdesign.net.OkHttpUtils;
+import com.nostra13.universalimageloader.cache.disc.impl.UnlimitedDiscCache;
+import com.nostra13.universalimageloader.cache.disc.naming.HashCodeFileNameGenerator;
+import com.nostra13.universalimageloader.cache.disc.naming.Md5FileNameGenerator;
+import com.nostra13.universalimageloader.core.ImageLoader;
+import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
+import com.nostra13.universalimageloader.core.assist.QueueProcessingType;
+import com.nostra13.universalimageloader.utils.StorageUtils;
 
+import java.io.File;
 import java.io.InputStream;
 import java.util.concurrent.TimeUnit;
 
@@ -13,8 +23,7 @@ import okio.Buffer;
 /**
  * Created by zhy on 15/8/25.
  */
-public class MyApplication extends Application
-{
+public class MyApplication extends Application {
     private String CER_12306 = "-----BEGIN CERTIFICATE-----\n" +
             "MIICmjCCAgOgAwIBAgIIbyZr5/jKH6QwDQYJKoZIhvcNAQEFBQAwRzELMAkGA1UEBhMCQ04xKTAn\n" +
             "BgNVBAoTIFNpbm9yYWlsIENlcnRpZmljYXRpb24gQXV0aG9yaXR5MQ0wCwYDVQQDEwRTUkNBMB4X\n" +
@@ -31,15 +40,65 @@ public class MyApplication extends Application
             "-----END CERTIFICATE-----";
 
     @Override
-    public void onCreate()
-    {
+    public void onCreate() {
         super.onCreate();
         OkHttpUtils.getInstance().setCertificates(new InputStream[]{
                 new Buffer()
                         .writeUtf8(CER_12306)
                         .inputStream()});
         OkHttpUtils.getInstance().debug("testDebug").getOkHttpClient().setConnectTimeout(100000, TimeUnit.MILLISECONDS);
+        initImageLoader(this);
 
+    }
 
+    /**
+     * 初始化ImageLoader
+     */
+
+    public static void initImageLoader(Context context) {
+        File cacheDir = StorageUtils.getOwnCacheDirectory(context,
+                "app/Cache");// 获取到缓存的目录地址
+        Log.e("cacheDir", cacheDir.getPath());
+        // 创建配置ImageLoader(所有的选项都是可选的,只使用那些你真的想定制)，这个可以设定在APPLACATION里面，设置为全局的配置参数
+        ImageLoaderConfiguration config = new ImageLoaderConfiguration.Builder(
+                context)
+                // max width, max height，即保存的每个缓存文件的最大长宽
+                .memoryCacheExtraOptions(480, 800)
+                        // Can slow ImageLoader, use it carefully (Better don't use it)设置缓存的详细信息，最好不要设置这个
+//				 .discCacheExtraOptions(480, 800, CompressFormat.JPEG, 75, null)
+                        // 线程池内加载的数量
+                .threadPoolSize(3)
+                        // 线程优先级
+                .threadPriority(Thread.NORM_PRIORITY - 2)
+                /*
+                 * When you display an image in a small ImageView
+				 *  and later you try to display this image (from identical URI) in a larger ImageView
+				 *  so decoded image of bigger size will be cached in memory as a previous decoded image of smaller size.
+				 *  So the default behavior is to allow to cache multiple sizes of one image in memory.
+				 *  You can deny it by calling this method:
+				 *  so when some image will be cached in memory then previous cached size of this image (if it exists)
+				 *   will be removed from memory cache before.
+				 */
+//				.denyCacheImageMultipleSizesInMemory()
+
+                        // You can pass your own memory cache implementation你可以通过自己的内存缓存实现
+                        // .memoryCache(new UsingFreqLimitedMemoryCache(2 * 1024 * 1024))
+                        // .memoryCacheSize(2 * 1024 * 1024)
+                        //硬盘缓存50MB
+                .diskCacheSize(50 * 1024 * 1024)
+                        //将保存的时候的URI名称用MD5
+                .diskCacheFileNameGenerator(new Md5FileNameGenerator())
+                        // 加密
+                .diskCacheFileNameGenerator(new HashCodeFileNameGenerator())//将保存的时候的URI名称用HASHCODE加密
+                .tasksProcessingOrder(QueueProcessingType.LIFO)
+                .diskCacheFileCount(100) //缓存的File数量
+                .diskCache(new UnlimitedDiscCache(cacheDir))// 自定义缓存路径
+                        // .defaultDisplayImageOptions(DisplayImageOptions.createSimple())
+                        // .imageDownloader(new BaseImageDownloader(context, 5 * 1000,
+                        // 30 * 1000)) // connectTimeout (5 s), readTimeout (30 s)超时时间
+                .writeDebugLogs() // Remove for release app
+                .build();
+        // Initialize ImageLoader with configuration.
+        ImageLoader.getInstance().init(config);// 全局初始化此配置
     }
 }
